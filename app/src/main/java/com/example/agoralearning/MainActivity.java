97f2f755118251dc96e664d8fad87b6fa4a1aede
uzairKhan <uzairsiddiqkhan@gmail.com>
2022-10-30 21:37:36 +0500

    initail commit

---
+++ app/src/main/java/com/example/agoralearning/MainActivity.java
@@ -0,0 +1,151 @@
+package com.example.agoralearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Bundle;
+import io.agora.rtc2.Constants;
+import io.agora.rtc2.IRtcEngineEventHandler;
+import io.agora.rtc2.RtcEngine;
+import io.agora.rtc2.RtcEngineConfig;
+import io.agora.rtc2.ChannelMediaOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String appId = "abd7dda083264ee8ab256962fd567f4b";
+    private String channelName = "khanuzair";
+    private String token = "";
+    private int uid = 0;
+    // Track the status of your connection
+    private boolean isJoined = false;
+
+    // Agora engine instance
+    private RtcEngine agoraEngine;
+    // UI elements
+    private TextView infoText;
+    private Button joinLeaveButton;
+
+    private static final int PERMISSION_REQ_ID = 22;
+    private static final String[] REQUESTED_PERMISSIONS =
+            {
+                    Manifest.permission.RECORD_AUDIO
+            };
+
+    private boolean checkSelfPermission()
+    {
+        if (ContextCompat.checkSelfPermission(this, REQUESTED_PERMISSIONS[0]) !=  PackageManager.PERMISSION_GRANTED)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    void showMessage(String message) {
+        runOnUiThread(() ->
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show());
+    }
+
+    private void setupVoiceSDKEngine() {
+        try {
+            RtcEngineConfig config = new RtcEngineConfig();
+            config.mContext = getBaseContext();
+            config.mAppId = appId;
+            config.mEventHandler = mRtcEventHandler;
+            agoraEngine = RtcEngine.create(config);
+        } catch (Exception e) {
+            throw new RuntimeException("Check the error.");
+        }
+    }
+
+    private final IRtcEngineEventHandler mRtcEventHandler = new IRtcEngineEventHandler() {
+        @Override
+        // Listen for the remote user joining the channel.
+        public void onUserJoined(int uid, int elapsed) {
+            runOnUiThread(()->infoText.setText("Remote user joined: " + uid));
+        }
+
+        @Override
+        public void onJoinChannelSuccess(String channel, int uid, int elapsed) {
+            // Successfully joined a channel
+            isJoined = true;
+            showMessage("Joined Channel " + channel);
+            runOnUiThread(()->infoText.setText("Waiting for a remote user to join"));
+        }
+
+        @Override
+        public void onUserOffline(int uid, int reason) {
+            // Listen for remote users leaving the channel
+            showMessage("Remote user offline " + uid + " " + reason);
+            if (isJoined) runOnUiThread(()->infoText.setText("Waiting for a remote user to join"));
+        }
+
+        @Override
+        public void onLeaveChannel(RtcStats 	stats) {
+            // Listen for the local user leaving the channel
+            runOnUiThread(()->infoText.setText("Press the button to join a channel"));
+            isJoined = false;
+        }
+    };
+
+    private void joinChannel() {
+        ChannelMediaOptions options = new ChannelMediaOptions();
+        options.autoSubscribeAudio = true;
+
+        // Set both clients as the BROADCASTER.
+        options.clientRoleType = Constants.CLIENT_ROLE_BROADCASTER;
+        // Set the channel profile as BROADCASTING.
+        options.channelProfile = Constants.CHANNEL_PROFILE_LIVE_BROADCASTING;
+
+        // Join the channel with a temp token.
+        // You need to specify the user ID yourself, and ensure that it is unique in the channel.
+        agoraEngine.joinChannel(token, channelName, uid, options);
+
+    }
+
+
+    public void joinLeaveChannel(View view) {
+        if (isJoined) {
+            agoraEngine.leaveChannel();
+            joinLeaveButton.setText("Join");
+        } else {
+            joinChannel();
+            joinLeaveButton.setText("Leave");
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // If all the permissions are granted, initialize the RtcEngine object and join a channel.
+        if (!checkSelfPermission()) {
+            ActivityCompat.requestPermissions(this, REQUESTED_PERMISSIONS, PERMISSION_REQ_ID);
+        }
+
+        setupVoiceSDKEngine();
+
+        // Set up access to the UI elements
+        joinLeaveButton = findViewById(R.id.joinLeaveButton);
+        infoText = findViewById(R.id.infoText);
+    }
+    protected void onDestroy() {
+        super.onDestroy();
+        agoraEngine.leaveChannel();
+
+        // Destroy the engine in a sub-thread to avoid congestion
+        new Thread(() -> {
+            RtcEngine.destroy();
+            agoraEngine = null;
+        }).start();
+    }
+
+
+
+}
